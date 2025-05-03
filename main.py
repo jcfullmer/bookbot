2510d49bd36750e4d16c190cbeb9ee1cc727e3d2
jcfullmer <clayefullmer@gmail.com>
2025-05-02 21:00:54 -0400

    added letter count func

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from stats import get_num_words
+from stats import letter_count
+
 def get_book_text(f):
     with open(f) as frank:
         return frank.read()
@@ -7,9 +10,7 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
+    letters = letter_count(text)
+    print(letters)
 
 main()
---
+++ stats.py
@@ -0,0 +1,14 @@
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def letter_count(text):
+    letters_count = {}
+    for i in text:
+        lowercase_text = text.lower()
+        if i not in letters_count:
+            letters_count[i] = 1
+        if i in letters_count:
+            letters_count[i] =+ 1
+    return letters_count
+
