25cd4ff2abaab6c70cda1d8109356db14696edaa
jcfullmer <clayefullmer@gmail.com>
2025-05-03 00:21:31 -0400

    got it working not sure how

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from stats import get_num_words
 from stats import letter_count
 
 def get_book_text(f):
-    with open(f) as frank:
+    with open(f, encoding="utf8") as frank:
         return frank.read()
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
+    print(len(text))
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
     letters = letter_count(text)
     print(letters)
+    print("done")
 
 main()
--- stats.py
+++ stats.py
@@ -4,12 +4,12 @@ def get_num_words(text):
 
 def letter_count(text):
     letters_count = {}
-    
-    for i in text:
-        lowercase_text = text.lower()
-        if i not in letters_count:
-            letters_count[i] = 1
-        if i in letters_count:
-            letters_count[i] =+ 1
+    lowercase_text = text.lower()
+    for i in lowercase_text:
+        if i.isalpha():
+            if i in letters_count:
+                letters_count[i] += 1
+            else:
+                letters_count[i] = 1
     return letters_count
 
