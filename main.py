9872d35ac87167b70b4f92026cff681672d5e4eb
jcfullmer <clayefullmer@gmail.com>
2025-05-03 20:14:12 -0400

    final bookbot!

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from stats import get_num_words
 from stats import letter_count
 from stats import sort_alpha
+import sys
 
 def get_book_text(f):
     with open(f, encoding="utf8") as frank:
         return frank.read()
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     letter_counted = letter_count(text)
@@ -29,15 +30,8 @@ def main():
     
     print("============= END ===============")
 
-'''def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    print(len(text))
-    num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
-    letter_counted = letter_count(text)
-    print(letter_counted)
-    sorted = sort_alpha(letter_counted)
-    print(sorted)
-'''
-main()
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else: 
+    main()
